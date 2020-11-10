025fc355b7f3e19f5f628117e00b52327990c543
jacy <fmp_higherfly@foxmail.com>
2020-11-11 00:01:55 +0800

    first commit

---
+++ mall-tiny/src/main/java/com/jacky/mall/tiny/service/UmsMemberService.java
@@ -0,0 +1,15 @@
+package com.jacky.mall.tiny.service;
+
+import com.jacky.mall.tiny.common.api.CommonResult;
+
+public interface UmsMemberService {
+    /**
+     * 生成验证码
+     */
+    CommonResult generateAuthCode(String telephone);
+
+    /**
+     * 判断验证码和手机号码是否匹配
+     */
+    CommonResult verifyAuthCode(String telephone, String authCode);
+}
